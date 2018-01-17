e770376e0b3f8bc5f7b7eeb3ea070fb7edb6e827
Alexey Romanov <jgc128@outlook.com>
2018-01-17 10:27:04 -0500

    variable works with np arrays

--- pytorch_helpers/helpers.py
+++ pytorch_helpers/helpers.py
@@ -20,6 +20,9 @@ def variable(obj, volatile=False):
     if isinstance(obj, (list, tuple)):
         return [variable(o, volatile=volatile) for o in obj]
 
+    if isinstance(obj, np.ndarray):
+        obj = torch.from_numpy(obj)
+
     obj = cuda(obj)
     obj = Variable(obj, volatile=volatile)
     return obj
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytorch_helpers",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=['numpy', 'opencv-python', 'imgaug'],
     author="Alexey Romanov",
